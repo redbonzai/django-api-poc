d1d5c82ab27a5920bb6052fa364b87fb882e8b28
Christian Dominguez <christian@redbonzai.com>
2023-03-28 18:50:51 -0500

    postgres-sql-adapter

--- app/app/calc.py
+++ app/app/calc.py
@@ -6,3 +6,8 @@ Caluculator functions
 def add(x, y):
     """Add X + y and return the result"""
     return x + y
+
+
+def subtract(x, y):
+    """Subtract X - y and return the result"""
+    return x - y
